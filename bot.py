b2e83e500d1f3fc51a8db172b5f6ae1fd30d7523
devilhazretleri <emoamas2ci@gmail.com>
2024-06-19 00:06:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,8 +6,14 @@ api_id = '21871272'
 api_hash = '57efa4949cd41dccd628c04b8507ff2b'
 phone_number = '+12563655354'
 
-welcome_message = 'Merhaba! Botumuza hoş geldiniz. Size nasıl yardımcı olabilirim?'
-sent_messages = set()  # Gönderilen mesajları takip etmek için bir set kullanıyoruz
+# Kullanıcının mesajlarına verilecek yanıtlar
+response_map = {
+    0: 'Merhaba! Nasıl yardımcı olabilirim?',
+    1: 'Fiyat listesi: Example 2500₺, 5000₺'
+}
+
+# Kullanıcıların yanıt sırasını takip etmek için bir sözlük kullanıyoruz
+user_messages_count = {}
 
 async def main():
     # TelegramClient'i başlatma
@@ -19,9 +25,20 @@ async def main():
     @client.on(events.NewMessage)
     async def handler(event):
         sender = await event.get_sender()
-        if sender.id not in sent_messages:
-            await event.reply(welcome_message)
-            sent_messages.add(sender.id)
+        sender_id = sender.id
+        message_text = event.raw_text.lower()
+
+        # Kullanıcının mesaj sayısını al veya sıfırla
+        if sender_id not in user_messages_count:
+            user_messages_count[sender_id] = 0
+        else:
+            user_messages_count[sender_id] += 1
+
+        # Yanıt verilecek mesajı bul ve gönder
+        response_index = user_messages_count[sender_id]
+        if response_index in response_map:
+            response = response_map[response_index]
+            await event.reply(response)
 
     print("Bot çalışıyor...")
     await client.run_until_disconnected()
