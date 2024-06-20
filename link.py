45b8dce9deff09639140d48373f516f782f83a8b
devilhazretleri <emoamas2ci@gmail.com>
2024-06-20 19:57:50 +0300

    Create link.py

---
+++ link.py
@@ -0,0 +1,57 @@
+from telethon import TelegramClient, events, Button
+import asyncio
+
+# API kimlik bilgilerinizi buraya yazın
+api_id = '27777717'
+api_hash = '7591c98b80d35f58d380e68e526ac589'
+phone_number = '+905318635833'
+
+# Her bir saatlik mesaj
+hourly_message = (
+    "💗 KENDİME AİT YERİM YOK 💗\n\n"
+    "🔥SEVGİLİ TADINDA GÖRÜŞÜYORUM 🔥\n\n"
+    "🏠 EVE. OTELE. APARTA REZiDANS. GELiYORUM\n\n"
+    "💸 ÜCRET ELDEN\n\n"
+    "❗️ÖNDEN ÖDEME YOK\n\n"
+    "Sevgili Tadinda Guven Ve Kalite Ön Planda"
+)
+
+# Gönderilecek fotoğrafın yolu
+photo_path = 'fotoğraf.jpg'
+
+# Buton ile yönlendirilecek link
+button_url = "https://mersinescort.netlify.app"
+button_text = "Detaylar için tıklayın"
+
+# Grupların kullanıcı adlarını `gruplar.txt` dosyasından oku
+with open('gruplar.txt', 'r') as file:
+    group_usernames = [line.strip() for line in file.readlines()]
+
+async def send_hourly_message(client):
+    while True:
+        print("Belirtilen gruplara mesaj gönderiliyor...")
+        for group_username in group_usernames:
+            try:
+                entity = await client.get_entity(group_username)
+                await client.send_message(entity, hourly_message, buttons=[Button.url(button_text, button_url)])
+                await client.send_file(entity, photo_path, caption=hourly_message)
+                print(f"{group_username} grubuna mesaj ve fotoğraf gönderildi.")
+            except Exception as e:
+                print(f"{group_username} grubuna mesaj ve fotoğraf gönderilemedi: {e}")
+        await asyncio.sleep(3600)  # 1 saat bekle
+
+async def main():
+    # TelegramClient'i başlatma
+    client = TelegramClient('session_name', api_id, api_hash)
+
+    # Giriş yapma
+    await client.start(phone_number)
+
+    # Saatlik mesajları göndermek için arka plan görevini başlatma
+    asyncio.create_task(send_hourly_message(client))
+
+    print("Bot çalışıyor...")
+    await client.run_until_disconnected()
+
+if __name__ == '__main__':
+    asyncio.run(main())
